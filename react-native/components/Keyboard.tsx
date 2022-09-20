1ccd7b0ae2163df98e79d7b2a86a975a5669473c
alexle <alexle@stud.ntnu.no>
2022-09-21 01:44:49 +0200

    update keyboard component

--- react-native/components/Keyboard.tsx
+++ react-native/components/Keyboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, Button, View, PixelRatio } from "react-native";
+import { StyleSheet, Text, Button, View, PixelRatio, Pressable } from "react-native";
 import { Employee, useFetchEmployees } from "../hooks/useFetchEmployees";
 import { RootTabScreenProps } from "../types";
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,43 +10,105 @@ export const Keyboard = () => {
   return (
     <View style={keyboardStyle.container}>
         <View style={keyboardStyle.row}>
-            <Text style={keyboardStyle.keys}>Q</Text>
-            <Text style={keyboardStyle.keys}>W</Text>
-            <Text style={keyboardStyle.keys}>E</Text>
-            <Text style={keyboardStyle.keys}>R</Text>
-            <Text style={keyboardStyle.keys}>T</Text>
-            <Text style={keyboardStyle.keys}>Y</Text>
-            <Text style={keyboardStyle.keys}>U</Text>
-            <Text style={keyboardStyle.keys}>I</Text>
-            <Text style={keyboardStyle.keys}>O</Text>
-            <Text style={keyboardStyle.keys}>P</Text>
-            <Text style={keyboardStyle.keys}>Å</Text>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            
         </View>
         <View style={keyboardStyle.row}>
-            <Text style={keyboardStyle.keys}>A</Text>
-            <Text style={keyboardStyle.keys}>A</Text>
-            <Text style={keyboardStyle.keys}>S</Text>
-            <Text style={keyboardStyle.keys}>D</Text>
-            <Text style={keyboardStyle.keys}>T</Text>
-            <Text style={keyboardStyle.keys}>F</Text>
-            <Text style={keyboardStyle.keys}>G</Text>
-            <Text style={keyboardStyle.keys}>H</Text>
-            <Text style={keyboardStyle.keys}>J</Text>
-            <Text style={keyboardStyle.keys}>Ø</Text>
-            <Text style={keyboardStyle.keys}>Æ</Text>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            
         </View>
         <View style={keyboardStyle.row}>
-            <Text style={keyboardStyle.keys}>Q</Text>
-            <Text style={keyboardStyle.keys}>W</Text>
-            <Text style={keyboardStyle.keys}>E</Text>
-            <Text style={keyboardStyle.keys}>R</Text>
-            <Text style={keyboardStyle.keys}>T</Text>
-            <Text style={keyboardStyle.keys}>Y</Text>
-            <Text style={keyboardStyle.keys}>U</Text>
-            <Text style={keyboardStyle.keys}>I</Text>
-            <Text style={keyboardStyle.keys}>O</Text>
-            <Text style={keyboardStyle.keys}>P</Text>
-            <Text style={keyboardStyle.keys}>Å</Text>
+            <Pressable style={keyboardStyle.keySpecial}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.key}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
+            <Pressable style={keyboardStyle.keySpecial}>
+                <Text style={keyboardStyle.keyText}>Q</Text>
+            </Pressable>
         </View>
     </View>
   );
@@ -54,29 +116,39 @@ export const Keyboard = () => {
 
 const keyboardStyle = StyleSheet.create({
     container: {
-        flex: 3.5,
+        flex: 3,
         flexDirection: 'column',
         justifyContent: 'space-around',
+        alignItems: 'center',
         backgroundColor: 'red',
-        paddingHorizontal: '3%',
+        paddingHorizontal: '1%',
     },
     row: {
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        padding: '2%',
+        padding: '1%',
         backgroundColor: 'red',
+        width: '100%',
     },
-    keys: {
-        fontSize: 23, //PixelRatio.get()*6,
+    key: {
         flex: 1,
-        textAlign: "center",
         backgroundColor: 'green',
-        marginHorizontal: '2%',
+        marginHorizontal: '0.5%',
+        paddingVertical: "3.5%",
+        
+    },
+    keySpecial: {
+        flex: 2,
+        backgroundColor: 'blue',
+        marginHorizontal: '1%',
+        paddingVertical: "3.5%",
+    },
+    keyText: {
+        fontSize: 23, //PixelRatio.get()*6
+        textAlign: "center",
     }
 });
-
-
 
 export default Keyboard;

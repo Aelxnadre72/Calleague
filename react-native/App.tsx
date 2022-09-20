e72abc8fae67cde88d0ffa0f537efda48240ce8f
alexle <alexle@stud.ntnu.no>
2022-09-21 00:01:30 +0200

    fix statusbar margintop

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import {StartScreen} from'./screens/StartScreen';
 import {PlayScreen} from'./screens/PlayScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initialWindowMetrics, SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,15 +19,15 @@ export default function App() {
     return null;
   } else {
     return (
+      <>
+      <StatusBar backgroundColor='#d4f2fc'/>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Start">
           <Stack.Screen name = "Start" component={StartScreen} />
           <Stack.Screen name = "Play" component={PlayScreen} />
-          
-         
-
         </Stack.Navigator>
       </NavigationContainer>
+      </>
     );
   }
 }
